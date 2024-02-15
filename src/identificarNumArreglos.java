634c285587b0133ad323da47e603f26583e2c9ec
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-02-15 16:08:17 -0600

    se agrego la clase identificar numero similares en dos arreglos

---
+++ src/identificarNumArreglos.java
@@ -0,0 +1,11 @@
+public class identificarNumArreglos {
+    public static void main(String[] args) {
+        int arr1[] = {1,3,3,4,5,6,8,9,9};
+        int arr2[] = {2,2,3,5,7,9};
+        int arr3[];
+        int i=0;
+        while (i < arr1.length) {
+            
+        }
+    }
+}
